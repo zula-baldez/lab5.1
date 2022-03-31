1a62eaedae1e005a5b3ce2be16398091e7470df6
Egor <moyakkauntda@gmail.com>
2022-03-31 17:07:47 +0300

    some updates

---
+++ lab-server/src/main/java/zula/server/ServerApp.java
@@ -0,0 +1,42 @@
+package zula.server;
+
+import zula.common.data.ServerMessage;
+import zula.common.exceptions.PrintException;
+import zula.common.util.IoManager;
+import zula.server.commands.Save;
+import zula.server.commands.ServerExit;
+import zula.server.util.ListManager;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class ServerApp {
+    private final Logger appLogger = Logger.getLogger("App logger");
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void startApp(IoManager ioManager, ObjectInputStream in, ListManager listManager) throws IOException, ClassNotFoundException, PrintException {
+        while (ioManager.isProcessStillWorks()) {
+            if (System.in.available() > 0) {
+                String command = scanner.nextLine();
+                if ("exit".equals(command)) {
+                    ServerExit serverExit = new ServerExit();
+                    serverExit.execute(ioManager, listManager, "");
+                    appLogger.info("До свидания!");
+                    continue;
+                }
+                if ("save".equals(command)) {
+                    Save save = new Save();
+                    save.execute(ioManager, listManager);
+                    appLogger.info("Команда выполнена!");
+                }
+            }
+                ServerMessage serverMessage = (ServerMessage) in.readObject();
+                serverMessage.getCommand().execute(ioManager, listManager, serverMessage.getArguments());
+            appLogger.info("Успешное выполнение команды");
+
+
+        }
+    }
+}
